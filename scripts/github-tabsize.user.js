412bdcc244148cace5a069eaf8045d5d9e85bb53
zkqiang <zkqiang@126.com>
2021-04-14 20:53:37 +0800

    Change ES5 syntax

--- scripts/github-tabsize.user.js
+++ scripts/github-tabsize.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name               Change tab-size on GitHub
+// @name               Change tab size on GitHub
 // @name:zh-CN         修改 GitHub tab 宽度
 // @namespace          https://github.com/zkqiang
-// @version            1.1
-// @description        Change tab-size from 8 to 4 on GitHub
+// @version            1.2
+// @description        Change tab width from 8 to 4 on GitHub
 // @description:zh-CN  将 GitHub 代码区域的 tab 宽度从 8 修改为 4
 // @author             zkqiang
 // @match              *://github.com/*
@@ -14,10 +14,10 @@
 (function () {
   'use strict';
 
-  document.querySelectorAll('table[data-tab-size]').forEach(node => {
-    node.setAttribute('data-tab-size', '4')
+  document.querySelectorAll('table[data-tab-size]').forEach(function (node) {
+    node.setAttribute('data-tab-size', '4');
   });
-  document.querySelectorAll('table.highlight').forEach(node => {
-    node.style['tab-size'] = '4'
+  document.querySelectorAll('table.highlight').forEach(function (node) {
+    node.style['tab-size'] = '4';
   });
 })();
