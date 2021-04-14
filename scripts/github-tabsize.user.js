0d2b79c97b51be6f766749c2c31a6c2ce075df4a
zkqiang <zkqiang@126.com>
2021-04-14 20:45:33 +0800

    Catch another case

--- scripts/github-tabsize.user.js
+++ scripts/github-tabsize.user.js
@@ -2,7 +2,7 @@
 // @name               Change tab-size on GitHub
 // @name:zh-CN         修改 GitHub tab 宽度
 // @namespace          https://github.com/zkqiang
-// @version            1.0
+// @version            1.1
 // @description        Change tab-size from 8 to 4 on GitHub
 // @description:zh-CN  将 GitHub 代码区域的 tab 宽度从 8 修改为 4
 // @author             zkqiang
@@ -16,5 +16,8 @@
 
   document.querySelectorAll('table[data-tab-size]').forEach(node => {
     node.setAttribute('data-tab-size', '4')
+  });
+  document.querySelectorAll('table.highlight').forEach(node => {
+    node.style['tab-size'] = '4'
   });
 })();
