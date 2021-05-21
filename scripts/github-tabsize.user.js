9319686bdbed406108b416a406609166bd8b7f97
zkqiang <zkqiang@126.com>
2021-05-21 12:27:48 +0800

    Run after each pjax load

--- scripts/github-tabsize.user.js
+++ scripts/github-tabsize.user.js
@@ -2,7 +2,7 @@
 // @name               Change tab size on GitHub
 // @name:zh-CN         修改 GitHub tab 宽度
 // @namespace          https://github.com/zkqiang
-// @version            1.2
+// @version            1.3
 // @description        Change tab width from 8 to 4 on GitHub
 // @description:zh-CN  将 GitHub 代码区域的 tab 宽度从 8 修改为 4
 // @author             zkqiang
@@ -14,10 +14,15 @@
 (function () {
   'use strict';
 
-  document.querySelectorAll('table[data-tab-size]').forEach(function (node) {
-    node.setAttribute('data-tab-size', '4');
-  });
-  document.querySelectorAll('table.highlight').forEach(function (node) {
-    node.style['tab-size'] = '4';
-  });
+  function change() {
+    document.querySelectorAll('table[data-tab-size]').forEach(function (node) {
+      node.setAttribute('data-tab-size', '4');
+    });
+    document.querySelectorAll('table.highlight').forEach(function (node) {
+      node.style['tab-size'] = '4';
+    });
+  }
+
+  change();
+  document.addEventListener('pjax:end', change);
 })();
