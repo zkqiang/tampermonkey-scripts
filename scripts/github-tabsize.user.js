60ee792a0a5d710524452016c382fddb9fa6910c
zkqiang <zkqiang@126.com>
2021-07-07 16:08:47 +0800

    Modify highlight selector

--- scripts/github-tabsize.user.js
+++ scripts/github-tabsize.user.js
@@ -2,7 +2,7 @@
 // @name               Change tab size on GitHub
 // @name:zh-CN         修改 GitHub tab 宽度
 // @namespace          https://github.com/zkqiang
-// @version            1.3
+// @version            1.4
 // @description        Change tab width from 8 to 4 on GitHub
 // @description:zh-CN  将 GitHub 代码区域的 tab 宽度从 8 修改为 4
 // @author             zkqiang
@@ -18,7 +18,7 @@
     document.querySelectorAll('table[data-tab-size]').forEach(function (node) {
       node.setAttribute('data-tab-size', '4');
     });
-    document.querySelectorAll('table.highlight').forEach(function (node) {
+    document.querySelectorAll('.highlight').forEach(function (node) {
       node.style['tab-size'] = '4';
     });
   }
